193d8dc8310e611098e2f3d795a33329c56348b1
Ahmed Nussair <anussair@gmail.com>
2021-04-27 13:48:38 +0200

    Project model updated.

--- lib/model/response/category.dart
+++ lib/model/response/category.dart
@@ -4,7 +4,6 @@ import 'package:json_annotation/json_annotation.dart';
 class Category {
   int id;
   String name;
-  @JsonKey(name: 'image_url')
   String imageUrl;
 
   Category(this.id, this.name, this.imageUrl);
@@ -12,12 +11,13 @@ class Category {
   factory Category.fromJson(Map<String, dynamic> json) => Category(
         json['id'] as int,
         json['name'] as String,
-        json['image_url'] as String,
+        json['imageUrl'] as String,
       );
 
-  Map<String, dynamic> toJson() => <String, dynamic>{
+  Map<String, dynamic> toJson() =>
+      <String, dynamic>{
         'id': id,
         'name': name,
-        'image_url': imageUrl,
+        'imageUrl': imageUrl,
       };
 }
