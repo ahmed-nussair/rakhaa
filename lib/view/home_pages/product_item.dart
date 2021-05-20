210c0f3404c6afd88fd02da2ef981807fb6df4ec
Ahmed Nussair <anussair@gmail.com>
2021-05-20 14:28:03 +0200

    Products page is enhanced.

--- lib/view/home_pages/product_item.dart
+++ lib/view/home_pages/product_item.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:rakhaa/view/home_pages/product_item_widgets/shopping_cart_icon.dart';
+import 'product_item_widgets/shopping_cart_icon.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../screen_util.dart';
 import 'offer_ribbon.dart';
 import 'product_item_widgets/wish_list_icon.dart';
+import '../../globals.dart' as Globals;
 
 class ProductItem extends StatelessWidget {
   final int id;
@@ -26,110 +28,123 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _screenUtil.init(context);
-    return GestureDetector(
-      onTap: () {},
-      child: Material(
-        elevation: _screenUtil.setWidth(5),
-        shadowColor: Colors.black,
-        child: Stack(
-          children: [
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                    imageUrl,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(_screenUtil.setHeight(10)),
-                  child: Container(
-                    height: _screenUtil.setHeight(150),
-                    child: Text(
-                      title,
-                      textAlign: TextAlign.center,
-                      maxLines: 2,
-                      style: TextStyle(
-                        fontSize: _screenUtil.setSp(40),
+    return FutureBuilder<SharedPreferences>(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          String token = snapshot.data.getString(Globals.token);
+          return GestureDetector(
+            onTap: () {},
+            child: Material(
+              elevation: _screenUtil.setWidth(5),
+              shadowColor: Colors.black,
+              child: Stack(
+                children: [
+                  Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Image.network(
+                          imageUrl,
+                          fit: BoxFit.fill,
+                        ),
                       ),
-                      overflow:
-                          TextOverflow.ellipsis, // so that text ends with dots
-                    ),
-                  ),
-                ),
-                Divider(),
-                Container(
-                  height: _screenUtil.setHeight(80),
-                  child: price == afterDiscount
-                      ? Padding(
-                          padding: EdgeInsets.all(_screenUtil.setHeight(10)),
+                      Padding(
+                        padding: EdgeInsets.all(_screenUtil.setHeight(10)),
+                        child: Container(
+                          height: _screenUtil.setHeight(150),
                           child: Text(
-                            ' جنيه ${price.toStringAsFixed(2)}',
+                            title,
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
                             style: TextStyle(
                               fontSize: _screenUtil.setSp(40),
                             ),
+                            overflow: TextOverflow
+                                .ellipsis, // so that text ends with dots
+                          ),
+                        ),
+                      ),
+                      Divider(),
+                      Container(
+                        height: _screenUtil.setHeight(80),
+                        child: price == afterDiscount
+                            ? Padding(
+                                padding:
+                                    EdgeInsets.all(_screenUtil.setHeight(10)),
+                                child: Text(
+                                  ' جنيه ${price.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                    fontSize: _screenUtil.setSp(40),
+                                  ),
+                                ),
+                              )
+                            : Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.all(
+                                        _screenUtil.setHeight(10)),
+                                    child: Text(
+                                      ' جنيه${afterDiscount.toStringAsFixed(2)}',
+                                      style: TextStyle(
+                                        fontSize: _screenUtil.setSp(40),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(
+                                        _screenUtil.setHeight(10)),
+                                    child: Text(
+                                      ' جنيه${price.toStringAsFixed(2)}',
+                                      style: TextStyle(
+                                        decoration: TextDecoration.lineThrough,
+                                        fontSize: _screenUtil.setSp(40),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                      ),
+                    ],
+                  ),
+                  token == null || token == ''
+                      ? Container()
+                      : Positioned(
+                          top: _screenUtil.setHeight(20),
+                          left: _screenUtil.setWidth(20),
+                          child: Column(
+                            children: [
+                              WishListIcon(
+                                id: id,
+                              ),
+                              SizedBox(
+                                height: _screenUtil.setHeight(30),
+                              ),
+                              ShoppingCartIcon(id: id),
+                            ],
+                          ),
+                        ),
+                  percentage > 0
+                      ? Positioned(
+                          top: 0.0,
+                          right: 0.0,
+                          child: Container(
+                            height: _screenUtil.setWidth(180),
+                            width: _screenUtil.setWidth(180),
+                            child: OfferRibbon(
+                              percentage: percentage,
+                            ),
                           ),
                         )
-                      : Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Padding(
-                              padding:
-                                  EdgeInsets.all(_screenUtil.setHeight(10)),
-                              child: Text(
-                                ' جنيه${afterDiscount.toStringAsFixed(2)}',
-                                style: TextStyle(
-                                  fontSize: _screenUtil.setSp(40),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  EdgeInsets.all(_screenUtil.setHeight(10)),
-                              child: Text(
-                                ' جنيه${price.toStringAsFixed(2)}',
-                                style: TextStyle(
-                                  decoration: TextDecoration.lineThrough,
-                                  fontSize: _screenUtil.setSp(40),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                ),
-              ],
-            ),
-            Positioned(
-              top: _screenUtil.setHeight(20),
-              left: _screenUtil.setWidth(20),
-              child: Column(
-                children: [
-                  WishListIcon(
-                    id: id,
-                  ),
-                  SizedBox(
-                    height: _screenUtil.setHeight(30),
-                  ),
-                  ShoppingCartIcon(id: id),
+                      : Container(),
                 ],
               ),
             ),
-            percentage > 0
-                ? Positioned(
-                    top: 0.0,
-                    right: 0.0,
-                    child: Container(
-                      height: _screenUtil.setWidth(180),
-                      width: _screenUtil.setWidth(180),
-                      child: OfferRibbon(
-                        percentage: percentage,
-                      ),
-                    ),
-                  )
-                : Container(),
-          ],
-        ),
-      ),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
--- lib/view/home_pages/products_page.dart
+++ lib/view/home_pages/products_page.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../screen_util.dart';
 
+import '../signin.dart';
 import 'product_item.dart';
+import '../../globals.dart' as Globals;
 
 class ProductsPage extends StatelessWidget {
   final String subCategoryName;
@@ -44,30 +48,88 @@ class ProductsPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color(0xff4599d7),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(_screenUtil.setWidth(50)),
-        child: GridView.count(
-          shrinkWrap: true,
-          scrollDirection: Axis.vertical,
-          crossAxisCount: 2,
-          childAspectRatio: (itemWidth / itemHeight),
-          crossAxisSpacing: _screenUtil.setWidth(40),
-          mainAxisSpacing: _screenUtil.setWidth(40),
-          children: List.generate(productsList.length, (index) {
-            return ProductItem(
-              id: productsList[index].id,
-              title: productsList[index].name,
-              imageUrl: productsList[index].imageUrl,
-              price: productsList[index].price,
-              afterDiscount: productsList[index].price -
-                  (productsList[index].price *
-                      productsList[index].discount /
-                      100),
-              percentage: productsList[index].discount,
-              // onMerchantNameTapped: widget.onMerchantNameTapped,
+      body: FutureBuilder<SharedPreferences>(
+        future: SharedPreferences.getInstance(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            String token = snapshot.data.getString(Globals.token);
+            return Column(
+              children: [
+                token == null || token == ''
+                    ? Container(
+                        height: _screenUtil.setHeight(250),
+                        width: MediaQuery.of(context).size.width,
+                        alignment: Alignment.center,
+                        color: Colors.blue,
+                        child: Padding(
+                          padding: EdgeInsets.all(_screenUtil.setWidth(10)),
+                          child: Container(
+                            width: _screenUtil.setWidth(1000),
+                            child: RichText(
+                              textAlign: TextAlign.end,
+                              text: TextSpan(children: [
+                                TextSpan(
+                                  text: 'عزيزي الزائر .. يجب عليك ',
+                                  style: TextStyle(
+                                    fontSize: _screenUtil.setSp(45),
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'تسجيل الدخول ',
+                                  style: TextStyle(
+                                    fontSize: _screenUtil.setSp(45),
+                                    decoration: TextDecoration.underline,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () => Navigator.of(context)
+                                        .pushReplacement(MaterialPageRoute(
+                                            builder: (context) => SignIn())),
+                                ),
+                                TextSpan(
+                                  text: 'أولًا لكي تتمكن من الشراء',
+                                  style: TextStyle(
+                                    fontSize: _screenUtil.setSp(45),
+                                  ),
+                                ),
+                              ]),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Container(),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(_screenUtil.setHeight(30)),
+                    child: GridView.count(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      crossAxisCount: 2,
+                      childAspectRatio: (itemWidth / itemHeight),
+                      crossAxisSpacing: _screenUtil.setWidth(40),
+                      mainAxisSpacing: _screenUtil.setWidth(40),
+                      children: List.generate(productsList.length, (index) {
+                        return ProductItem(
+                          id: productsList[index].id,
+                          title: productsList[index].name,
+                          imageUrl: productsList[index].imageUrl,
+                          price: productsList[index].price,
+                          afterDiscount: productsList[index].price -
+                              (productsList[index].price *
+                                  productsList[index].discount /
+                                  100),
+                          percentage: productsList[index].discount,
+                          // onMerchantNameTapped: widget.onMerchantNameTapped,
+                        );
+                      }),
+                    ),
+                  ),
+                ),
+              ],
             );
-          }),
-        ),
+          }
+          return Container();
+        },
       ),
     );
   }
